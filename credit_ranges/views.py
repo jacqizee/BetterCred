c3ead5c7e10d68dc5550e3e1a2df5bb465d8271f
Jacqueline <jhjzhou@gmail.com>
2022-06-08 17:31:58 -0400

    adding views to cashbackcategories, networks, and credit_ranges

--- credit_ranges/views.py
+++ credit_ranges/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import CreditRange
+from .serializers.common import CreditRangeSerializer
 
 # Create your views here.
+
+class CashBackView(APIView):
+
+    # GET Request - return all cash back categories
+    def get(self, _request):
+        serialized_categories = CreditRangeSerializer(CreditRange.objects.all(), many=True)
+        return Response(serialized_categories.data)
--- networks/views.py
+++ networks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Network
+from .serializers.common import NetworkSerializer
 
 # Create your views here.
+
+class CashBackView(APIView):
+
+    # GET Request - return all cash back categories
+    def get(self, _request):
+        serialized_categories = NetworkSerializer(Network.objects.all(), many=True)
+        return Response(serialized_categories.data)
