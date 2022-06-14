42184e27f7cdd25beab66fe0a4fda07e9d99bf95
Jacqueline <jhjzhou@gmail.com>
2022-06-14 09:31:16 -0400

    updated user model, pfp as optional

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -7,5 +7,5 @@ class User(AbstractUser):
     email = models.EmailField(max_length=50, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    profile_picture = models.CharField(max_length=300)
+    profile_picture = models.CharField(max_length=300, blank=True)
     wallet = models.ManyToManyField('credit_cards.CreditCard', related_name='users', blank=True)
