5036d80b198f7a484c994edf66898a33b5d6806a
Jacqueline <jhjzhou@gmail.com>
2022-06-21 11:41:17 -0400

    ready for deployment

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Environment Variables
 import environ
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
