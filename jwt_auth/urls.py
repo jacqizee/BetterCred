b11ea5f62a8c1f55a647bca7ca436156de036551
Jacqueline <jhjzhou@gmail.com>
2022-07-28 14:18:09 -0400

    adding testing for user login

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -4,7 +4,7 @@ from .views import RegisterView, LoginView, ProfileView, WalletView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user-register'),
-    path('login/', LoginView.as_view(), name='user_login'),
+    path('login/', LoginView.as_view(), name='user-login'),
     path('profile/<int:pk>/', ProfileView.as_view()),
     path('profile/<int:pk>/wallet/', WalletView.as_view()),
 ]
