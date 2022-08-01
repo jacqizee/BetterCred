2c760019581eccc8f5dc5d48b51dfe75b65ea566
Jacqueline <jhjzhou@gmail.com>
2022-07-31 21:17:00 -0400

    adding unit tests for networks model

--- networks/tests.py
+++ networks/tests.py
@@ -1,11 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Network
 
 # Create your tests here.
 class TestNetworks(TestCase):
     def setUp(self):
-        network = Network.objects.create(name="New Network", logo='https://logo-url.com')
+        Network.objects.create(name="New Network", logo='https://logo-url.com')
+        Network.objects.create(name="New Network 2", logo='https://logo-url2.com')
+        self.req_url = reverse('networks')
 
     def test_name(self):
         network = Network.objects.get(name="New Network")
-        self.assertEqual(network.name, "New Network")
+        self.assertEqual(network.logo, "https://logo-url.com")
+
+    def test_get_networks(self):
+        # Test that GET request returns networks
+        response = self.client.get(self.req_url)
+        self.assertEqual(len(response.data), 2)
+
+        # The HTTP request status code
+        self.assertEqual(response.status_code, 200)
+
+        # Check the populated Credit Card field is present
+        self.assertTrue('credit_cards' in response.data[0].keys())
--- networks/urls.py
+++ networks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import NetworkView
 
 urlpatterns = [
-    path('', NetworkView.as_view())
+    path('', NetworkView.as_view(), name='networks')
 ]
